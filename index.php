d0419bdbe791461b12f846b93eb5002637e44f3b
alexa <alexa@Crow>
2020-07-13 13:38:22 -0500

    cambio

--- index.php
+++ index.php
@@ -5,7 +5,7 @@
 	<link rel="stylesheet" type="text/css" href="css/estilos.css">
 </head>
 <body>
-	<h1>Esta es una prueba con Git</h1>
+	<h1>Esta es una prueba con Gitd</h1>
 <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
 tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam,
 quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo
